089aa07b6b6009875e2d225adc5c11d095b127fb
marsacom <kuklabrayden79@gmail.com>
2024-09-09 21:09:56 -0400

    Initial commit to publish to github, no functionality

---
+++ main.py
@@ -0,0 +1,18 @@
+# K2 - Scanner 
+# V.1.0.0
+# Author : Brayden Kukla
+
+import os
+import sys
+import socket
+import requests
+import datetime
+import threading
+import nmap as n 
+import subprocess
+
+def main():
+    pass
+
+def get_ips():
+    pass
