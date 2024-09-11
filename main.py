57f32665cc3e0a771e0bc9071783941c22908c8f
Brayden Kukla <brayden@ssisurplus.com>
2024-09-11 17:07:57 -0400

    Blah

--- main.py
+++ main.py
@@ -5,14 +5,20 @@
 import os
 import sys
 import socket
-import requests
 import datetime
+import argparse
+import requests
 import threading
 import nmap as n 
 import subprocess
 
 def main():
     pass
+
+def get_args():
+    parser = argparse.ArgumentParser(description="An accurate, fast, in depth network scanning tool")
+
+    parser.add_argument()
 
 def get_ips():
     pass
