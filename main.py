4e90722ff9142d48d9f69b8a5d03aef612be18e6
Brayden Kukla <brayden@ssisurplus.com>
2024-09-13 16:47:16 -0400

    Blah

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ import threading
 import nmap as n 
 import subprocess
 
+#Define the needed vars
+ps = n.PortScanner()
+
 def main():
     scan(args.target)
+    print(args.target)
 
 def get_args():
     parser = argparse.ArgumentParser(prog='K2-Scanner', description='An accurate, fast, in depth network scanning tool')
@@ -25,8 +29,17 @@ def get_args():
     return args
 
 def scan(ip):
-    ps = n.PortScanner()
-    ps.scan(hosts=ip)
+    ps.scan(ip, sudo=True)
+
+    # Print out results
+    for host in ps.all_hosts():
+        print("Host: ", host)
+        print("State: ", ps[host].state())
+        for protocol in ps[host].all_protocols():
+            print("Protocol: ", protocol)
+            ports = ps[host][protocol].keys()
+            for port in ports:
+                print("Port: ", port, "State: ", ps[host][protocol][port]['state'])
 
 args = get_args()
 main()
